09af6a1ac5d1c3374e427c5243c36c20739ad3bc
9ete <pete@petelower.com>
2021-02-13 12:23:32 -0800

    feature/initial-updates: add slick slider

---
+++ frontend/js/slick-slider.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ *
+ * 
+ *
+ * 
+ */
+
+( function( $ ) {
+
+	$(document).ready(function(){
+		$('.slick-slider-container').slick({
+			arrows: false,
+			autoplay: true,
+			autoplaySpeed: 4000,
+			slidesToShow: 1,
+			slidesToScroll: 1
+		});
+	});
+
+} )( jQuery );
